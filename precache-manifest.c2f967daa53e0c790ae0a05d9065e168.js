ac227673bf9b5df0f4073b26f302c37e2e9bb718
Pie-3 <jishan.ahamed7k@gmail.com>
2023-01-20 13:31:01 +0530

    Updates

---
+++ precache-manifest.c2f967daa53e0c790ae0a05d9065e168.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca42841c87e5d847b60d877d55999986",
+    "url": "/Taaza-Khabar/index.html"
+  },
+  {
+    "revision": "adac59318b121a13ff14",
+    "url": "/Taaza-Khabar/static/css/main.f8c71d40.chunk.css"
+  },
+  {
+    "revision": "366fdb5f775f23101eec",
+    "url": "/Taaza-Khabar/static/js/2.7b540a3f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Taaza-Khabar/static/js/2.7b540a3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adac59318b121a13ff14",
+    "url": "/Taaza-Khabar/static/js/main.5968fe0d.chunk.js"
+  },
+  {
+    "revision": "298598c9b993c3a801e1",
+    "url": "/Taaza-Khabar/static/js/runtime-main.28f32a86.js"
+  },
+  {
+    "revision": "19ec54d6e23edd366877f708dcdf7696",
+    "url": "/Taaza-Khabar/static/media/sun-logo.19ec54d6.png"
+  }
+]);
